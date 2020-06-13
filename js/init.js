81c56bc8acc83ae478e05f133001fa790db83a4c
Ratan Kadam <ratankadam@Ratans-MacBook-Pro.local>
2020-06-12 22:18:47 -0400

    minor

--- js/init.js
+++ js/init.js
@@ -38,6 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
     addEventListeners(myMap, store.cordinatesMapping);
     selfLocate(myMap);
     loadMain(myMap);
+
     loadUsaStats(myMap);
     loadIndiaStats(myMap);
   });
